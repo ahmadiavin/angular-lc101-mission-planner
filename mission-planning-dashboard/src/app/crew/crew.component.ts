6e47450516264b84be3e09d042cd43379f83b3e0
ahmadiavin <ahmadiavin@gmail.com>
2020-05-26 20:15:24 -0400

    studio 2 complete

--- mission-planning-dashboard/src/app/crew/crew.component.ts
+++ mission-planning-dashboard/src/app/crew/crew.component.ts
@@ -12,9 +12,26 @@ export class CrewComponent implements OnInit {
 
   ngOnInit() {
   }
-  crew: string[] =["Jessica Watkins", "Raja Chari", "Jasmin Moghbeli"];
-  counter(i:number){
-    return new Array(i);
+  crew: object[] = [
+    {name: "Eileen Collins", firstMission: false},
+    {name: "Mae Jemison", firstMission: false},
+    {name: "Ellen Ochoa", firstMission: true}
+ ];
+  memberBeingEdited: object = null;
+
+  add(memberName: string, isFirst: boolean) {
+    this.crew.push({name: memberName, firstMission: isFirst});
   }
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+  edit(member: object) {
+    this.memberBeingEdited = member;
+ }
+ save(name: string, member: object) {
+  member['name'] = name;
+  this.memberBeingEdited = null;
+}
 
 }
