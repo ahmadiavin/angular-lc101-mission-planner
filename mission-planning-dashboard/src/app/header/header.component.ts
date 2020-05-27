da4027f70703f2063481b11772de979b31e9acbb
ahmadiavin <ahmadiavin@gmail.com>
2020-05-26 20:30:53 -0400

    studio2 finished

--- mission-planning-dashboard/src/app/header/header.component.ts
+++ mission-planning-dashboard/src/app/header/header.component.ts
@@ -9,13 +9,9 @@ export class HeaderComponent implements OnInit {
   missionName: string = "Mars 2030";
   rocketName: string = "Plasma Max";
 
-<<<<<<< HEAD:mission-planning-dashboard/src/app/header/header.component.ts
-=======
-  missionName: string = "Mars 2030";
-  rocketName: string = "Plasma Max";
   editingMission: boolean = false;
+  editingRocket: boolean = false;
   
->>>>>>> 4c035bb29399ab83ad8449fd192b08e7e3b43303:src/app/header/header.component.ts
   constructor() { }
 
   ngOnInit() {
@@ -24,6 +20,10 @@ export class HeaderComponent implements OnInit {
   updateMission(updatedName: string) {
     this.missionName = updatedName;
     this.editingMission = false;
+  }
+  updateRocket(updatedRocket: string){
+    this.rocketName = updatedRocket;
+    this.editingRocket = false;
   }
 
 }
